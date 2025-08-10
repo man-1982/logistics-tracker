cb07dd83baf7597a6d606526d76a935af76d4272
Andrii Kocherhin <andriikocherhin@webadmins-Macbook-Air.local>
2025-08-10 00:54:12 -0700

    Implemented Redux provider and Query client provider

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,3 +1,21 @@
-// add this near the top of app/src/main.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { Provider } from "react-redux"; // Redux provider
+import { store } from "./store";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
+import "./index.css";
 import { CONFIG } from "./lib/config";
+
 console.info("[ENV OK]", CONFIG);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
+  </React.StrictMode>
+);

56c0166955cf24bdecbaa7d15a38dd16ba0c80bd
Andrii Kocherhin <andriikocherhin@webadmins-Macbook-Air.local>
2025-08-10 17:26:42 -0700

    Adds telemetry reducer.

--- app/src/store/index.ts
+++ app/src/store/index.ts
@@ -2,16 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch, useSelector } from "react-redux";
 import type { TypedUseSelectorHook } from "react-redux";
 import { authReducer } from "./authSlice";
+import { telemetryReducer } from "./telemetrySlices";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    telemetry: telemetryReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-// Typed hooks
+// pre-typed version for the redux hooks
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

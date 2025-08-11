bfc599766081fb8532d28af54f5ce5b79a047c40
Andrii Kocherhin <andriikocherhin@webadmins-Macbook-Air.local>
2025-08-10 17:30:04 -0700

    Adds telemetry slice for driver position management.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,23 @@
 import { useEffect, useRef, useState } from "react";
 import LoginForm from "./features/auth/LoginForm";
 import DriversList from "./features/drivers/DriversList";
-import { useAppSelector } from "./store";
+import {useAppDispatch, useAppSelector} from "./store";
 import { selectToken } from "./store/authSlice";
 import { connectWs, type WsEnvelope } from "./lib/ws";
+import {resetPositions, upsertPosition} from "./store/telemetrySlices.ts";
+import LiveMap from "./features/map/LiveMap.tsx";
 
 export default function App() {
   const token = useAppSelector(selectToken);
   const [log, setLog] = useState<string[]>([]);
   const wsRef = useRef<WebSocket | null>(null);
 
+  const d = useAppDispatch();
+
   useEffect(() => {
     if (!token) {
+      // reset all and good day by now!
+      d(resetPositions());
       if (wsRef.current) {
         wsRef.current.close();
         wsRef.current = null;
@@ -19,13 +25,23 @@ export default function App() {
       return;
     }
     const ws = connectWs((msg: WsEnvelope) => {
+      if(msg.type === "gps"){
+        // todo use implemented types. Refactor
+        const p = msg.data as {driverId: string, lat: number, lng: number};
+        //console.log(p);
+        // TODO should be ts into driver data or as is?
+        d(upsertPosition({
+          driverId: p.driverId,
+          pos: {lat: p.lat, lng: p.lng, ts: msg.ts}}
+        ));
+      }
       if (msg.type === "gps" || msg.type === "hello") {
         setLog((l) => [JSON.stringify(msg), ...l].slice(0, 5));
       }
     });
     wsRef.current = ws;
     return () => ws.close();
-  }, [token]);
+  }, [token, d]);
 
   if (!token) return <LoginForm/>;
 
@@ -33,6 +49,7 @@ export default function App() {
     <div className="grid gap-4 p-4">
       <h2 className="text-xl font-semibold">Dashboard</h2>
       <DriversList/>
+      <LiveMap/>
       <section>
         <h3 className="text-lg font-medium">WS last messages</h3>
         <pre className="max-h-52 overflow-auto bg-gray-100 p-2 rounded">{log.join("\n")}</pre>
